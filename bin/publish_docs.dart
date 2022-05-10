f75d350ed7c1ff318616483eb45f5206193348ed
Philip Cohn-Cort <phil@monstar-lab.com>
2022-05-10 09:47:59 -0400

    feat: Throw an error early if the doc/assets/ directory is missing

--- bin/publish_docs.dart
+++ bin/publish_docs.dart
@@ -1,10 +1,32 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
+import 'dart:io';
+
 import 'package:publish_docs/publish_docs.dart';
 
 /// Entrypoint to the 'publish_docs' library.
 ///
 /// This is what command-line callers like `dart pub run publish_docs` call.
 Future<void> main(List<String> arguments) async {
+  final assetsAbsolutePath = absolutePath('doc', 'assets');
+
+  _checkForAssetsDirectory(assetsAbsolutePath);
+
   await generateDocs(arguments);
+}
+
+/// We only run if the host project has created a 'doc/assets/' directory.
+void _checkForAssetsDirectory(String assetsAbsolutePath) {
+  final assetsDirectory = Directory(assetsAbsolutePath);
+
+  final assetsDirectoryUri = assetsDirectory.uri;
+
+  if (assetsDirectory.existsSync()) {
+    print('Using assets from $assetsDirectoryUri.');
+  } else {
+    throw UnsupportedError("""
+Please make sure to create a 'doc/assets/' directory within your project source.
+We expected to see one at $assetsDirectoryUri
+""");
+  }
 }
