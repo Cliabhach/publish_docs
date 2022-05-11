675aa51190b241398737f7469171ab452fe91348
Philip Cohn-Cort <phil@monstar-lab.com>
2022-05-11 11:59:08 -0400

    refactor: Rename update_gh_pages.dart -> gh_pages_update.dart

--- lib/publish_docs.dart
+++ lib/publish_docs.dart
@@ -1,5 +1,5 @@
 library publish_docs;
 
 export 'src/entrypoint.dart';
-export 'src/operation/update_gh_pages.dart';
+export 'src/operation/gh_pages_update.dart';
 export 'src/util/path_util.dart';
--- lib/src/operation/operation.dart
+++ lib/src/operation/operation.dart
@@ -1,3 +1,4 @@
 export 'assets_meta_provider.dart';
 export 'docs_bridge.dart';
 export 'gh_pages_patch.dart';
+export 'gh_pages_update.dart';
