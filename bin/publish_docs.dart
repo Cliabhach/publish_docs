a968600aef7977507555cf6e518283c82b73aefa
Philip Cohn-Cort <phil@monstar-lab.com>
2022-05-11 02:21:10 -0400

    feat: Print helpful message when you run bin/publish_docs

--- bin/publish_docs.dart
+++ bin/publish_docs.dart
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
-import 'dart:io';
-
 import 'package:publish_docs/publish_docs.dart';
-import 'package:publish_docs/src/util/util.dart';
 
 /// Entrypoint to the 'publish_docs' library.
 ///
@@ -12,6 +9,20 @@ Future<void> main(List<String> arguments) async {
   final assetsAbsolutePath = absolutePath('doc', 'assets');
 
   checkForAssetsDirectory(assetsAbsolutePath);
+
+  print(r'''
+Publish docs that match your project's theme and style.
+
+To just generate the dartdoc output, run
+$ dart pub run publish_docs:generate
+or
+$ flutter pub run publish_docs:generate
+
+To generate new docs and then apply them to your gh-pages branch, run
+$ dart pub run publish_docs:commit
+or
+$ flutter pub run publish_docs:commit
+''');
 
   await generateDocs(arguments);
 }
