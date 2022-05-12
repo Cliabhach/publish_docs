d81bd9326a4d06904d40a81821d68899ca8a5fcf
Philip Cohn-Cort <cliabhach@gmail.com>
2022-05-12 06:09:01 -0400

    refactor: Move two private functions in analyzer_util.dart to their own file

--- lib/src/util/analyzer_util.dart
+++ lib/src/util/analyzer_util.dart
@@ -3,7 +3,6 @@
 
 import 'dart:async';
 import 'dart:io' as io;
-import 'dart:isolate';
 
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/file_system/file_system.dart' as fs;
@@ -12,6 +11,7 @@ import 'package:analyzer/file_system/physical_file_system.dart';
 import 'package:dartdoc/dartdoc.dart';
 import 'package:path/path.dart' as path;
 
+import 'package:publish_docs/src/private/assets_layers.dart';
 import 'package:publish_docs/src/private/mime_type.dart';
 
 /// A modified copy of [pubPackageMetaProvider].
@@ -22,8 +22,8 @@ Future<PackageMetaProvider> obtainPackageMetaProvider() async {
   final provider = await obtainOverlayProvider(
       pathForLayers: assetsAbsolutePath,
       layers: [
-        assetsAbsolutePath // First entry in this array should always be this directory
-      ] + await _publishDocsResourceLayers() // Ordered by decreasing importance
+        assetsAbsolutePath // First entry in this array should always be this
+      ] + await publishDocsResourceLayers() // Order by decreasing importance
     );
 
   final sdkDir = PhysicalResourceProvider.INSTANCE
@@ -38,30 +38,6 @@ Future<PackageMetaProvider> obtainPackageMetaProvider() async {
     provider,
     sdkDir,
     messageForMissingPackageMeta: messageForMissingMeta);
-}
-
-/// Method inspired by some of the private 'ResourceLoader' code in `dartdoc`.
-///
-/// Delegates some of the work to [_uriAsPath].
-Future<List<String>> _publishDocsResourceLayers() async {
-  final provider = PhysicalResourceProvider.INSTANCE;
-  const ourPackage = 'package:publish_docs';
-  final base = _uriAsPath('$ourPackage/resources/dartdoc-5.1.0', provider);
-  return [ await base ];
-}
-
-/// Find the [fs.Folder] on disk that represents a given `package:` [uri].
-Future<String> _uriAsPath(String uri, fs.ResourceProvider provider) async {
-  final resourcesUri = Uri.parse(uri);
-  final resolvedUri = await Isolate.resolvePackageUri(resourcesUri);
-
-  if (resolvedUri == null) {
-    throw UnsupportedError('The publish_docs package must be present in your '
-        "pub-cache in order for it to work properly. We can't load default "
-        'resources or assets otherwise.');
-  } else {
-    return provider.getFolder(resolvedUri.toFilePath()).path;
-  }
 }
 
 /// Retrieve the absolute, root-anchored, filesystem path to a file/directory.
