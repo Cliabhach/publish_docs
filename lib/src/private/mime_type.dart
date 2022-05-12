2c62dd7dfdde65cbf30e7118a3f73133034067c6
Philip Cohn-Cort <cliabhach@gmail.com>
2022-05-12 06:09:01 -0400

    docs: Split out two more functions in mime_type.dart

--- lib/src/private/mime_type.dart
+++ lib/src/private/mime_type.dart
@@ -3,6 +3,7 @@
 import 'dart:io' as io;
 
 import 'package:analyzer/file_system/file_system.dart' as fs;
+import 'package:meta/meta.dart';
 import 'package:mime/mime.dart';
 import 'package:publish_docs/src/private/binary_code_page.dart';
 
@@ -47,7 +48,7 @@ class DocsMimeType {
 /// Figure out what mime type best reflects the given file.
 ///
 /// Check out the docs on [DocsMimeType.type] and MDN's guide at
-/// (this page)[https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types]
+/// [this page](https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types)
 /// for a crash course in what mime types actually are and why they matter.
 ///
 /// Note that the standard library's [lookupMimeType] is woefully incomplete,
@@ -70,32 +71,57 @@ Future<String?> _queryMimeType(fs.File file) async {
 
   if (io.Platform.isLinux || io.Platform.isMacOS || io.Platform.isAndroid) {
     // Try to use the host's 'xdg-mime' tool
-    final result = io.Process.runSync('xdg-mime', [
-      'query',
-      'filetype',
-      file.path
-    ]);
-
-    if (result.exitCode == 0) {
-      xdgMimeType = result.stdout as String;
-    }
+    xdgMimeType = checkWithHost(file);
   }
 
   if (xdgMimeType != null) {
     return xdgMimeType;
   } else {
     // 'xdg-mime' was unavailable or could not identify the file
-    final normalFile = io.File(file.path);
-    final firstFewBytes = await normalFile
-        .openRead(0, 12) // No need to read more bytes. Library only checks 12.
-        .reduce((previous, element) => previous + element);
-
-    final type = lookupMimeType(file.path, headerBytes: firstFewBytes);
-
-    if (type == null) {
-      print('> Got bytes $firstFewBytes.');
-    }
+    final type = await checkWithLibrary(file);
 
     return type;
   }
+}
+
+/// Calls 'xdg-mime query filetype' with the path of [file].
+///
+/// This check is more accurate than [checkWithLibrary] when identifying files
+/// with missing or inaccurate extensions.
+///
+/// See also [the xdg-mime manual](https://linux.die.net/man/1/xdg-mime).
+@visibleForTesting
+String? checkWithHost(fs.File file) {
+  final result = io.Process.runSync('xdg-mime', [
+    'query',
+    'filetype',
+    file.path
+  ]);
+
+  if (result.exitCode == 0) {
+    return result.stdout as String;
+  } else {
+    return null;
+  }
+}
+
+/// Calls [lookupMimeType] with the name of [file] and its first few bytes.
+///
+/// The quality of this check is directly related to how many types have been
+/// registered on the global [MimeTypeResolver]. If no additional types have
+/// been registered, identification for some simple files (LICENSE, .gitignore)
+/// will fail.
+@visibleForTesting
+Future<String?> checkWithLibrary(fs.File file) async {
+  final normalFile = io.File(file.path);
+  final firstFewBytes = await normalFile
+      .openRead(0, 12) // No need to read more bytes. Library only checks 12.
+      .reduce((previous, element) => previous + element);
+
+  final type = lookupMimeType(file.path, headerBytes: firstFewBytes);
+
+  if (type == null) {
+    print('> Got bytes $firstFewBytes.');
+  }
+  return type;
 }
