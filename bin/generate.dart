f0d655e7847cfabf46088f4f50882595e710f4d5
Philip Cohn-Cort <cliabhach@gmail.com>
2022-05-12 06:09:02 -0400

    feat: Create two additional command-line scripts

---
+++ bin/generate.dart
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
+import 'package:publish_docs/publish_docs.dart';
+import 'package:publish_docs/src/operation/docs_bridge.dart';
+import 'package:publish_docs/src/private/assets_resource_provider.dart';
+
+/// Entrypoint to the 'publish_docs' library.
+///
+/// Invocation: `dart pub run publish_docs:generate`
+///
+/// Further details: [generateDocs], [getDartdocWithAssets],
+/// [obtainAssetsProvider].
+Future<void> main(List<String> arguments) async {
+  final assetsAbsolutePath = absolutePath('doc', 'assets');
+
+  checkForAssetsDirectory(assetsAbsolutePath);
+
+  await generateDocs(arguments);
+}
