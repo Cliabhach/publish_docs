68433a2395a1550231a93adb288a914535cbc3a6
Philip Cohn-Cort <cliabhach@gmail.com>
2022-06-13 23:17:30 -0400

    test: Add one test of BranchUpdate::defineVersion

--- test/src/branch_update_test.dart
+++ test/src/branch_update_test.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: prefer_const_constructors, directives_ordering, avoid_print
 
 import 'package:mocktail/mocktail.dart';
+import 'package:path/path.dart' as path;
 import 'package:publish_docs/src/git/commands.dart';
 import 'package:publish_docs/src/operation/branch_update.dart';
 import 'package:test/test.dart';
+
+import 'test_util.dart';
 
 
 class MockGitCommands extends Mock implements GitCommands {
@@ -28,9 +31,11 @@ void main() {
   group('branch_update', () {
     late GitCommands git;
     late List<String> logMessages;
+    late String testResourcePath;
     setUp(() {
       git = MockGitCommands();
       logMessages = [];
+      testResourcePath = resourcePath('branch_update');
     });
     test('basic check of logStatus', () async {
       // Given
@@ -43,6 +48,26 @@ void main() {
       // Then
       expect(logMessages, isNotEmpty);
       expect(logMessages.first, 'Update: Running for some-branch');
+    });
+    test('version is based on git data', () async {
+      // Given
+      final update = SimpleBranchUpdate(git, logMessages);
+      // (use a pubspec that we've prepared for this test)
+      final testProjectPath = path.absolute(testResourcePath, 'test1');
+      when(() => git.path).thenReturn(testProjectPath);
+      // (don't actually check the git revision; use this hash instead)
+      when(() => git.revParse(any())).thenAnswer((_) => Future.value('test1'));
+
+      // When
+      final versionString = await update.defineVersion();
+
+      // Then
+      verify(() => git.revParse(any())).called(1);
+      expect(logMessages, isNotEmpty);
+      // (we use parentheses to indicate lower-priority messages)
+      // (the actual contents aren't so important; we just expect one message)
+      expect(logMessages.first, stringContainsInOrder(['Update:', '(', ')']));
+      expect(versionString, '1.2.3+test1');
     });
   });
 }
