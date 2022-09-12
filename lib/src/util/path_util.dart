9f0a8abfb6bd9c32921443687dfaec5ad55aaf6a
Philip Cohn-Cort <cliabhach@gmail.com>
2022-09-11 23:22:05 -0400

    feat: Add a function to path_util.dart for doc/assets/
    This is part of a larger set of changes to make that
    relative path into something more configurable.

--- lib/src/util/path_util.dart
+++ lib/src/util/path_util.dart
@@ -4,24 +4,43 @@ import 'dart:io';
 
 import 'package:analyzer/file_system/physical_file_system.dart';
 
+/// Return the absolute path to the current project's 'assets' directory.
+///
+/// That directory is where project-specific favicons and styles and so forth
+/// should be defined.
+///
+/// Note that this function is typically called _very_ early on, as part of the
+/// startup checks ([checkForAssetsDirectory] in particular). The default return
+/// value is '/path/to/project/doc/assets/'.
+///
+/// See also [absolutePath].
+String assetsPath() {
+  return absolutePath('doc', 'assets');
+}
+
 /// Retrieve the absolute, root-anchored, filesystem path to a file/directory.
 ///
-/// This is a simplified alias to [path.absolute], which has more detailed
-/// docs.
+/// This is a simplified alias to
+/// [path's Context.absolute](https://pub.dev/documentation/path/1.8.2/path/Context/absolute.html),
+/// which has more detailed docs.
 ///
 /// See also [PhysicalResourceProvider.pathContext].
 String absolutePath(String part1, [String? part2]) {
   return PhysicalResourceProvider.INSTANCE.pathContext.absolute(part1, part2);
 }
 
-/// We only run if the host project has created a 'doc/assets/' directory.
+/// We only run if the host project has created an assets directory.
 ///
 /// There are about 8 files defined in 'resourceNames' in dartdoc's
 /// [html_resources.g.dart](https://github.com/dart-lang/dartdoc/blob/26d38618/lib/src/generator/html_resources.g.dart).
 ///
-/// We will try to load each of those files from the 'docs/assets/' directory.
-/// Any that are _not_ present in that directory will instead be loaded from
-/// one of our fallback directories.
+/// We will try to load each of those files from the [assetsAbsolutePath]
+/// directory. Any that are _not_ present in that directory will instead be
+/// loaded from one of our fallback directories.
+///
+/// Callers are asked to only use [assetsPath] as the parameter to this
+/// function. It is possible that the explicit parameter will be removed in a
+/// future release of this library.
 ///
 /// See `lib/src/private/assets_layers.dart` for the exact paths to our
 /// fallback directories.
